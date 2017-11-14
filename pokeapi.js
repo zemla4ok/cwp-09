0a9abbfb760545ca95db00c27e2e3f787e03cf6b
zemla4ok <dima-vital@live.com>
2017-11-14 18:13:21 +0300

    get 10 pokemons, items and locations

--- pokeapi.js
+++ pokeapi.js
@@ -24,10 +24,32 @@ Promise.all(result)
     })
 */
 //1, 4, 7
+/*
 Promise.any([
     axios.get('http://pokeapi.co/api/v2/pokemon/1'),
     axios.get('http://pokeapi.co/api/v2/pokemon/4'),
     axios.get('http://pokeapi.co/api/v2/pokemon/7')
 ]).then((result) => {
     console.log(`first is: ${result.data.name}`);
+})
+*/
+//10 pokemons, items and locations
+
+Promise.props({
+    pokemons: axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=10`),
+    items: axios.get(`https://pokeapi.co/api/v2/item/?limit=10`),
+    locations: axios.get(`https://pokeapi.co/api/v2/location/?limit=10`)
+}).then((result) => {
+    console.log("POKEMONS");
+    result["pokemons"].data.results.forEach((val) => {
+        console.log(val.name);
+    });
+    console.log("ITEMS");
+    result["items"].data.results.forEach((val) => {
+        console.log(val.name);
+    });
+    console.log("LOCATIONS");
+    result["locations"].data.results.forEach((val) => {
+        console.log(val.name);
+    });
 })
