44e49c73d2fb1c47e88d70eb6434c47692879243
zemla4ok <dima-vital@live.com>
2017-11-14 14:57:46 +0300

    download 30 pokemons

--- pokeapi.js
+++ pokeapi.js
@@ -6,3 +6,17 @@ axios.get('http://pokeapi.co/api/v2/pokemon/42')
     .then((res) => {
         console.log(`name: ${res.data.name}, weight: ${res.data.weight}, height: ${res.data.height}`);
     })
+
+//download 30 pokemons
+let result = [];
+for(let i=0;i<3;i++){
+    result.push(axios.get('http://pokeapi.co/api/v2/pokemon/?limit=10'));
+}
+Promise.all(result)
+    .then((res) => {
+        res.forEach((val, resInd)=>{
+            val.data.results.forEach((pokemon, ind) => {
+                console.log(`${resInd}-${ind}: ${pokemon.name}`);
+            })
+        })
+    })
